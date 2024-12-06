cd3aecb28929f07542a4c4c911d86367a89dc125
table106 <cytrynek106@gmail.com>
2024-12-06 08:50:10 +0100

    Add basic files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # buckshotCPP
-oh god its multiplying
+this is the third time
+## you know the drill
+buckshot roulette, no graphics, yadda yadda
+### but why
+learning new languages and kinda want to try them out, this time its C++
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+                                                                            int main() {
+int main() {
+    cout << "epic intro\n";
+    cout << "v0.0\n";
+
+    return 0;
+}
