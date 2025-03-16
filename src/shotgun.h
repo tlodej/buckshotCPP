3948e90b26d821693bac06b177daae6b2f5e6c58
tlodej <tymoteuszlodej@gmail.com>
2025-03-16 21:01:34 +0100

    use enum instead of string (strings are bad)

--- src/shotgun.h
+++ src/shotgun.h
@@ -4,26 +4,34 @@
 #include <random>
 #include <algorithm>
 
+enum class Shell {
+    Blank,
+    Live
+};
+
 class Shotgun {
     public:
-        std::vector<std::string> content;
+        std::vector<Shell> content;
         Shotgun() {
             this->dmg = 1;
             this->content = {};
         }
-        std::string shell(int n=0) {
+
+        Shell shell(int n=0) {
             return this->content[n];
         }
+
         void shoot() {
             this->dmg = 1;
             this->content.erase(this->content.begin());
         }
+
         void insertShells(int lives, int blanks) {
             for (int i = 0; i < lives; i++) {
-                this->content.push_back("live");
+                this->content.push_back(Shell::Live);
             }
             for (int j = 0; j < blanks; j++) {
-                this->content.push_back("blank");
+                this->content.push_back(Shell::Blank);
             }
             auto rd = std::random_device {};
             auto rng = std::default_random_engine { rd() };
