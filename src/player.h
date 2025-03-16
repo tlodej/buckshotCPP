d8a6562ee482001583303518de99ba0931e41629
tlodej <tymoteuszlodej@gmail.com>
2025-03-16 21:06:39 +0100

    i dont think player should print game state. it should be handled separately

--- src/player.h
+++ src/player.h
@@ -14,17 +14,13 @@ class Player {
         int getLives() {
             return this->lives;
         }
-        friend std::ostream& operator<< (std::ostream &s, Player &player);
+        
         void addOpponent(Player opp) {
             this->opponents.push_back(opp);
         }
+
     private:
         int lives;
         int wins;
         std::vector<Player> opponents;
 };
-
-std::ostream& operator<<(std::ostream &s, Player &player) {
-    s << player.name << "'s turn\n" << "you have " << player.getLives() << " lives\n" << "type to use:\n" << "shoot - shotgun\n";
-    return s;
-}
