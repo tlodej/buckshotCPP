16f9e5cb8f9ff337050c4ca0599fe82dbac0c544
tlodej <tymoteuszlodej@gmail.com>
2025-03-16 21:32:49 +0100

    make main more readable

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,50 @@
-// #include <iostream>
-// #include <chrono>
-// #include <thread>
-// #include <fstream>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <fstream>
 #include "shotgun.h"
 #include "system.h"
-// #include "player.h"
+#include "player.h"
 
-using namespace std;
+void welcomeMessage() {
+    std::cout << 
+            "=======================\n"
+            "|| BUCKSHOT ROULETTE ||\n"
+            "||   (C++ version)   ||\n"
+            "=======================\n"
+            "v0.0-alpha\n"
+            "press enter to start\n"
+            "or type 'how' for a how-to-play\n"
+            "you can also check out the credits by typing 'creds'\n";
+}
+
+void commandPrompt() {
+    std::cout << "\n> ";
+    std::string command;
+    std::cin >> command;
+
+    if (command == "how") {
+        // TODO: create guide file and embed it with #include or smth
+    } else if (command == "creds") {
+        // TODO: create credits file and embed it with #include or smth
+    } else if (command == "test time") {
+        std::cout << "enter round and (int)3p\n";
+        int round;
+        std::cin >> round;
+
+        int threeP;
+        std::cin >> threeP;
+    }
+
+}
 
 //cout << *plr << endl; use to print name
 int main() {
-    // cout << "well hello there! welcome to...\n";
-    // this_thread::sleep_for(chrono::seconds(3));
+    welcomeMessage();
+    commandPrompt();
 
-    // cout << "=======================\n";
-    // cout << "|| BUCKSHOT ROULETTE ||\n";
-    // cout << "||   (C++ version)   ||\n";
-    // cout << "=======================\n";
-    // cout << "v0.0-alpha\n";
-    // cout << "press enter to start\n";
-    // cout << "or type 'how' for a how-to-play\n";
-    // query("you can also check out the credits by typing 'creds'");
+    Shotgun shotgun{};
 
-    // string ans;
-    // cin >> ans;
-    // Shotgun shotgun{};
-    // clear();
-    // if (ans == "how") {
-    //     string line;
-    //     ifstream file("guide.txt");
-    //     while (getline(file, line)) {
-    //         cout << line;
-    //     }
-    //     file.close();
-    //     cout << "when you finish reading, press enter\n";
-    //     cin >> line;
-    // } else if (ans == "creds") {
-    //     string line;
-    //     ifstream file("credits.txt");
-    //     while (getline(file, line)) {
-    //         cout << line;
-    //     }
-    //     file.close();
-    //     cout << "when you finish reading, press enter\n";
-    //     cin >> line;
-    // } else if (ans == "test time") {
-    //     cout << "enter round and (int)3p\n";
-    //     int round,threeP;
-    //     cin >> round;
-    //     cin >> threeP;
-        
-    // }
 
     return 0;
 }
